54ae7dc5bbe26542bf36473bb174f8f2d8fd074a
Mithun Nath Mayooranadhan <mithun-nath@outlook.com>
2017-03-19 01:25:34 -0400

    update

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from 'angular2/core';
+import {MySkills} from './skills.component'
 
-@Component({
+@Component ({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1> <my-skills></my-skills>'
+    directives: [MySkills]
 })
-export class AppComponent { }
+export class AppComponent {
+
+ }
